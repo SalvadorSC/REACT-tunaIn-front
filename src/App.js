9cf6b83feea8ccb41ea3d318209619d56c3eaeb5
IRIS <iris@MacBook-Air-de-Cristina.local>
2020-10-27 11:51:32 +0100

    Componente FormRegistro creado, estilos creados y empezados sin terminar. Implementada funcionalidad de prueba en un botón en App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FormRegistro from "./FormRegistro";
 
 function App() {
+  const openModal = () => {
+    console.log("open modal");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={openModal}>Regístrarte</button>
+        <FormRegistro />
         <a
           className="App-link"
           href="https://reactjs.org"
