f6ba676d403089713719ad1a08970ed369305df9
SalvaSC <sanchezcampossalvador@gmail.com>
2020-10-28 20:41:37 +0100

    Modificación en el archivo App.js Y en la funcionalidad de la pagina principal en base a lo que hemos aprendido hoy de React Router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,120 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import FormRegistro from "./FormRegistro";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+function HomeButton() {
+  let history = useHistory();
 
-
-function App() {
-  const formRegistroRef = React.useRef();
-
-  const openModal = () => {
-    formRegistroRef.current.abrirForm();
-  };
+  function handleClick() {
+    history.push("/home");
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={openModal}>Regístrarte</button>
-        <FormRegistro>
-          <h3>¡Personaliza tu experiencia!</h3>
-          <p>
-            <span>Disfruta de una experiencia sin interrupciones en todos los dispositivos y recomendaciones personalizadas basadas en tu escucha. (Solo toma 30 segundos)</span>
-          </p>
-          <button onClick={() => formRegistroRef.current.cerrarForm()}>Regístrate</button>
-        </FormRegistro>
-      </header>
+    <button type="button" onClick={handleClick}>
+      Go home
+    </button>
+  );
+}
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <HomeButton />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+function Profile() {
+  return <h2>Name</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+function Register() {
+  
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
