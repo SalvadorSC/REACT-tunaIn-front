ac3d19d8c07830e060fa991dface8174284883c0
IRIS <iris@MacBook-Air-de-Cristina.local>
2020-11-05 10:37:25 +0100

    Autorización añadido a urlBack

--- src/Components/urlBack.js
+++ src/Components/urlBack.js
@@ -1,16 +1,25 @@
 export const serverRequest = (resources, method, body) => {
+    const token = localStorage.getItem('token');
     const url = `http://localhost:3000/${resources}`;
     const JSONBody = JSON.stringify(body);
     const options = {
         method: method,
         mode: 'cors',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authoritation': token
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSONBody
     }
-    return fetch(url, options).then(response => response.json());
+    return fetch(url, options).then(response => {
+        if (response.status > 400) {
+            response.json();
+        } else {
+            console.log(response.status);
+        }
+
+    });
 }
 
 
