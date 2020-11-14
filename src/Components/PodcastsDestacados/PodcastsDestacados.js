86a419c92470ac3953acb6194bc33fae62162322
Cris <cristina.picatoste@gmail.com>
2020-11-14 01:24:51 +0100

    Componente Listado de Podcasts reutilizable y sección 4 de la home que muestra los podcasts destacados

--- src/Components/PodcastsDestacados/PodcastsDestacados.js
+++ src/Components/PodcastsDestacados/PodcastsDestacados.js
@@ -19,17 +19,42 @@ export const PodcastsDestacados = () => {
       duration: 3.33,
       author: "Autor podcast 3",
     },
+    {
+      title: "Soy el podcast 4",
+      duration: 3.33,
+      author: "Autor podcast 4",
+    },
+    {
+      title: "Soy el podcast 1",
+      duration: 1.3,
+      author: "Autor podcast 1",
+    },
+    {
+      title: "Soy el podcast 2",
+      duration: 2.3,
+      author: "Autor podcast 2",
+    },
+    {
+      title: "Soy el podcast 3",
+      duration: 3.33,
+      author: "Autor podcast 3",
+    },
+    {
+      title: "Soy el podcast 4",
+      duration: 3.33,
+      author: "Autor podcast 4",
+    },
   ];
   return (
-    <>
-      <PodcastCard />
-      {ListaPodcastFake.map((podc) => {
+    <div className="PodcastsDestados-wrap">
+      {ListaPodcastFake.map(podcast => 
+        (
         <PodcastCard
-          title={podc.title}
-          duration={podc.duration}
-          author={podc.author}
-        />;
-      })}
-    </>
+          title={podcast.title}
+          duration={podcast.duration}
+          author={podcast.author}
+        />
+      ))}
+    </div>
   );
 };
