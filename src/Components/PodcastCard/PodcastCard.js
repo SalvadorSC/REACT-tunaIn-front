aaf292169a26bb31a218a3dab653f2c2c08ae1a5
Cris <cristina.picatoste@gmail.com>
2020-11-14 01:40:08 +0100

    Corrección visibilidad iconos en componente PodcastsCards

--- src/Components/PodcastCard/PodcastCard.js
+++ src/Components/PodcastCard/PodcastCard.js
@@ -5,9 +5,9 @@ export const PodcastCard = ( {title, duration, author} ) => {
   return (
     <div className="PodcastCard-wrap">
       <div className="PodcastCard-img">
-        <i className="fas fa-share-alt icons" />
-        <i className="far fa-play-circle icons" />
-        <i className="far fa-heart icons" />
+        <i className="fas fa-share-alt icon" />
+        <i class="fas fa-play fa-2x icon"></i>
+        <i className="far fa-heart icon" />
       </div>
       <div className="PodcastCard-text">
         <p className="PostcastCard-title">{title}</p>
