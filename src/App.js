d232946b1babee609f31b06be848d9da4bf5cbc4
Cris <cristina.picatoste@gmail.com>
2020-11-16 14:28:33 +0100

    Ruta privada para que el usuario se logguee si no hay token y la página no pete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { UserProfile } from "./pages/userProfile/UserProfile";
 import { EditUserProfile } from "./pages/editUserProfile/EditUserProfile";
 import { RegisterForm } from "./pages/register/RegisterForm";
@@ -10,8 +10,17 @@ import { Home } from "./pages/home/home";
 import { Terms } from "./pages/terms/Terms";
 import { UserPodcastList } from "./pages/userPodcastList/UserPodcastList";
 import { HOME, USER, PODCAST, REGISTER, LOGIN, PROFILE, EDITUSERPROFILE, TERMS, USERPODCASTLIST } from "./routes/routes";
+import { getToken } from "./util/LocalStorage.utils";
 
 export default function App() {
+
+  const PrivateRoute = ({component: Component, path}) => (
+    <Route  
+      path={path}
+      render={() => getToken() ? <Component /> : <Redirect to={LOGIN}/>}
+    />
+  )
+  
 
   return (
     <Router>
@@ -21,11 +30,12 @@ export default function App() {
             {/* <Route path="/about" component={About} /> */}
             <Route exact path={REGISTER} component={RegisterForm} />
             <Route exact path={LOGIN} component={LogIn} />
-            <Route exact path={PROFILE} component={UserProfile} />
+            {/* <Route exact path={PROFILE} component={UserProfile} /> */}
             <Route exact path={EDITUSERPROFILE} component={EditUserProfile} />
             <Route exact path={HOME} component={Home} />
             <Route exact path={TERMS} component={Terms} />
             <Route exact path={USERPODCASTLIST} component={UserPodcastList} />
+            <PrivateRoute exact path={PROFILE} component={UserProfile} />
           </Switch>
         </div>
         <NavBar />
