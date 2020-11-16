a312d5469ebd1a87575c572cb7560bd0fd72d72d
Elis <elisalvarez@msn.com>
2020-11-16 14:59:59 +0100

    inicio ruta privada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 import { UserProfile } from "./pages/userProfile/UserProfile";
 import { EditUserProfile } from "./pages/editUserProfile/EditUserProfile";
 import { RegisterForm } from "./pages/register/RegisterForm";
@@ -10,6 +11,16 @@ import { Home } from "./pages/home/home";
 import { Terms } from "./pages/terms/Terms";
 import { UserPodcastList } from "./pages/userPodcastList/UserPodcastList";
 import { HOME, USER, PODCAST, REGISTER, LOGIN, PROFILE, EDITUSERPROFILE, TERMS, USERPODCASTLIST } from "./routes/routes";
+import {getToken} from "./util/LocalStorage.utils";
+
+const isLogin = () => getToken();
+
+const PrivateRoute = ({ component: Component, path }) => (
+    <Route path={path} render={() => {
+      return isLogin() ? <Component /> : <Redirect to="LOGIN" />;
+    }}
+    />
+);
 
 export default function App() {
 
@@ -22,7 +33,7 @@ export default function App() {
             <Route exact path={REGISTER} component={RegisterForm} />
             <Route exact path={LOGIN} component={LogIn} />
             <Route exact path={PROFILE} component={UserProfile} />
-            <Route exact path={EDITUSERPROFILE} component={EditUserProfile} />
+            <PrivateRoute path={EDITUSERPROFILE} component={EditUserProfile} />
             <Route exact path={HOME} component={Home} />
             <Route exact path={TERMS} component={Terms} />
             <Route exact path={USERPODCASTLIST} component={UserPodcastList} />
--- src/util/DecodeToken.js
+++ src/util/DecodeToken.js
@@ -1,7 +1,8 @@
 export const DecodeToken = (token) => {
 
-    const encToken = token.split('.')[1];
-    const tokenData = atob(encToken);
+  //  const encToken = token.split('.')[1];
+    const tokenData = atob(token);
+    console.log(token);
     const parsedTokenData = JSON.parse(tokenData);
 
     return parsedTokenData;
