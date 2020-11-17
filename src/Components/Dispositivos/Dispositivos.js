b97e5b3f360e65ab583860d007192e0d261acc01
Cris <cristina.picatoste@gmail.com>
2020-11-17 02:35:54 +0100

    Actualización en componente Dispositivos

--- src/Components/Dispositivos/Dispositivos.js
+++ src/Components/Dispositivos/Dispositivos.js
@@ -10,8 +10,8 @@ export const Dispositivos = () => {
             <div className="Dispositivos-text">
                 <h1>DISPONIBLE EN TODOS TUS DISPOSITIVOS</h1>
                 <p>Disfruta de tu audio exactamente donde, cuando y como quieras. TuneIn está disponible en cientos de dispositivos domésticos, de automóviles y portátiles, y funciona con Alexa y Google Home.</p>
-                <button type="button" className="download-apple"><i class="fab fa-apple"></i> Download</button>
-                <button type="button" className="download-google"><i class="fab fa-google-play"></i> Download</button>
+                <button type="button" className="download-apple"><i className="fab fa-apple"></i> Download</button>
+                <button type="button" className="download-google"><i className="fab fa-google-play"></i> Download</button>
             </div>
         </div>
     )
