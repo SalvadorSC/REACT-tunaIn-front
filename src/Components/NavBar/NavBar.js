16d8878f7c5008c3f5bbea78793ea8ef042e7217
SalvaSC <sanchezcampossalvador@gmail.com>
2020-11-19 20:58:45 +0100

    Ahora la navbar y el Container son responsive.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo-tuna-noname.svg"; // with import
 import React from "react";
 import "./NavBar.css";
 import { useState, useEffect } from "react";
 
 
-export function NavBar() {
+export function NavBar({ setContainerClass }) {
   const [navbarClass, setNavbarClass] = useState('');
   const url = window.location.href;
+  const location = useLocation();
 
   useEffect(() => {
-    if (url === "http://localhost:3000/") {
+    console.log(location);
+    if (location.pathname === "/") {
       setNavbarClass("navbar-home");
+      setContainerClass("container-home");
     }
     else {
       setNavbarClass("navbar");
+      setContainerClass("container");
     }
-  }, [url]);
+  }, [location]);
 
   return (
     <>
