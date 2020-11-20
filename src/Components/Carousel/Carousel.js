f784432b1d5de213556a09b5bb069888e0c3072d
Cris <cristina.picatoste@gmail.com>
2020-11-20 11:25:43 +0100

    Cambios de estilo de la home

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -6,6 +6,9 @@ import './Carousel.css';
 export const Carousel = () => {
 
     const imgArray = [
+        "https://images.unsplash.com/photo-1603502142164-9e3e61b01e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80",
+        "https://images.unsplash.com/photo-1482442120256-9c03866de390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "https://images.unsplash.com/photo-1534327737286-52d27a912124?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80",
         "https://via.placeholder.com/1903x678/141414/fff?text=1",
         "https://via.placeholder.com/1903x678/141414/fff?text=2",
         "https://via.placeholder.com/1903x678/141414/fff?text=3",
