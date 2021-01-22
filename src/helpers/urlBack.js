e8b48e8fd13f5f0d84409a60fe2d7808442d58da
SalvaSC <sanchezcampossalvador@gmail.com>
2021-01-22 20:31:37 +0100

    Actualizada la url del front para escuchar al backend.

--- src/helpers/urlBack.js
+++ src/helpers/urlBack.js
@@ -3,7 +3,7 @@ import { getToken, hasSession } from "../util/LocalStorage.utils";
 export const serverRequest = (resources, method, body) => {
     //const token = getToken();
     const token = hasSession() ? getToken() : '';
-    const API_URL = window.location.hostname === "http://www.tunain.com" ? "tunainblahblah url back" : `http://localhost:3300/`;
+    const API_URL = window.location.hostname === "http://www.tunain.com" ? "https://tunain3.herokuapp.com/" : `http://localhost:3300/`;
     /* const url = `http://localhost:3300/${resources}`; */
     const url =  `${API_URL}/${resources}`
     const JSONBody = JSON.stringify(body);
