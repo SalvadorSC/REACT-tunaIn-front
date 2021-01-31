9da69081402af890f099bcb67de647ddfabf6bbb
Elis Alvarez <elisalvarez@msn.com>
2021-01-31 17:54:13 +0100

    Implementando listado comments con fecha

--- src/pages/podcastInformation/PodcastInformation.js
+++ src/pages/podcastInformation/PodcastInformation.js
@@ -1,95 +1,152 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import { Link, useParams, useHistory } from "react-router-dom";
-import { serverRequest } from "../../helpers/urlBack";
+import {useEffect, useState} from "react";
+import {Link, useParams, useHistory} from "react-router-dom";
+import {serverRequest} from "../../helpers/urlBack";
 import {getUserId} from "../../util/LocalStorage.utils";
 import "./PodcastInformation.css";
-import { formatDistance, subDays } from 'date-fns';
+import {formatDistance, subDays, format} from 'date-fns';
+
 
 export const PodcastInformation = () => {
-  const [user, setUser] = useState({});
-  const [podcast, setPodcast] = useState({});
-  const [comment, setComment] = useState([]);
-  const history = useHistory();
-  const userId = getUserId();
-  const date = formatDistance(subDays(new Date(), 3), new Date())
-console.log(date);
-  function handleClick() {
-    history.push(`/editPodcastInformation/${podcastId}`);
-  }
+    const [user, setUser] = useState({});
+    const [podcast, setPodcast] = useState({});
+    const [comment, setComment] = useState([]);
+    const history = useHistory();
+    let {podcastId} = useParams();
+    let formatDate = "";
+    const userId = getUserId();
+    const date = new Date();
+    const [saveComment, setSaveComment] = useState({
+        comment: "",
+        user: userId,
+        podcast: podcastId,
+        date: date,
+    });
 
-  let { podcastId } = useParams();
+    function handleClick() {
+        history.push(`/editPodcastInformation/${podcastId}`);
 
-  useEffect(() => {
-    serverRequest(`data/podcast/${podcastId}`, "GET")
-      .then((response) => {
-        setPodcast(response)
-      })
-      .catch(console.log);
-  }, []);
-
-
-  useEffect(() => {
-
-    serverRequest(`data/user/${userId}`, "GET")
-      .then((response) => {
-        setUser(response);
-      })
-      .catch(console.log);
-  }, []);
-
-  useEffect(() => {
-
-    serverRequest(`comment/${podcastId}`, "GET")
-        .then((response) => {
-          setComment(response);
-          console.log(response);
-        })
-        .catch(console.log);
-  }, []);
-
-
-  const editPodcastLink = () => {
-    if (user._id === podcast.id_author) {
-      return (
-        <>
-          <div>
-            <Link onClick={handleClick}><button>Editar podcast</button></Link>
-          </div>
-        </>)
     }
-  }
 
-  return (
-    <>
-      <div className="UserProfile-wrap">
-        <br />
-        <div className="UserCard-wrap">
-          <div className={"UserInfo"}>
-            <h2>{podcast.title}</h2>
-            <div><p>Categories: <span className="podcastInformationDisplayed">{podcast.categories}</span></p></div>
-            <div className="UserSectionLine"><p>Description:</p></div>
-            <div className="UserSectionLine"><p className="podcastInformationDisplayed">{podcast.description}</p></div>
-          </div>
-          <div className={"vr"} />
-          <img className="profilePicture" alt="background" src="https://c0.anyrgb.com/images/434/137/recording-studio-person-woman-microphone-radio-podcast-talking-singing-presenter.jpg" />
-          <br />
-        </div>
-        <hr></hr>
-        <div>
-          {comment.map(comment => (
-          <div>
-            <h5>{`${user.nombre} escribió:`}</h5>
-            <p>{comment.comment}</p>
-            <hr className="hrModal"></hr>
-          </div>))
-          }
-        </div>
-        <div className="PodcastsUser-wrap">
-          <br />
-          {editPodcastLink()}
-        </div>
-      </div>
-    </>
-  );
+    const resetField = ()=>{
+        setSaveComment({
+            ...saveComment,
+                comment: ""
+            }
+        )
+
+    }
+    const handleChange = (e) => {
+        e.preventDefault();
+        const value = e.target.value;
+        setSaveComment({
+            ...saveComment,
+            [e.target.name]: value
+        });
+        console.log(saveComment);
+    }
+
+    useEffect(() => {
+        serverRequest(`data/podcast/${podcastId}`, "GET")
+            .then((response) => {
+                setPodcast(response)
+            })
+            .catch(console.log);
+    }, []);
+
+
+    useEffect(() => {
+
+        serverRequest(`data/user/${userId}`, "GET")
+            .then((response) => {
+                setUser(response);
+            })
+            .catch(console.log);
+    }, []);
+
+    useEffect(() => {
+
+        serverRequest(`comment/${podcastId}`, "GET")
+            .then((response) => {
+                setComment(response);
+                formatDate = format(response.date, "dd/MM/yyyy");
+                console.log(formatDate);
+            })
+            .catch(console.log);
+    }, []);
+
+    const submitNewComment = (e) => {
+        e.preventDefault();
+        if (!saveComment.comment) {
+            alert("Comment is missing");
+        } else {
+            serverRequest(`comment/${podcastId}`, "POST", saveComment)
+                .then((response) => {
+                    //mensaje success
+                    alert("Comentario Guardado con Exito");
+
+                })
+                .catch((response) => {
+                    console.log(response);
+                });
+        }
+    }
+
+    const editPodcastLink = () => {
+        if (user._id === podcast.id_author) {
+            return (
+                <>
+                    <div>
+                        <Link onClick={handleClick}>
+                            <button>Editar podcast</button>
+                        </Link>
+                    </div>
+                </>)
+        }
+    }
+
+    return (
+        <>
+            <div className="UserProfile-wrap">
+                <br/>
+                <div className="UserCard-wrap">
+                    <div className={"UserInfo"}>
+                        <h2>{podcast.title}</h2>
+                        <div><p>Categories: <span className="podcastInformationDisplayed">{podcast.categories}</span>
+                        </p></div>
+                        <div className="UserSectionLine"><p>Description:</p></div>
+                        <div className="UserSectionLine"><p
+                            className="podcastInformationDisplayed">{podcast.description}</p></div>
+                    </div>
+                    <div className={"vr"}/>
+                    <img className="profilePicture" alt="background"
+                         src="https://c0.anyrgb.com/images/434/137/recording-studio-person-woman-microphone-radio-podcast-talking-singing-presenter.jpg"/>
+                    <br/>
+                </div>
+                <hr></hr>
+                <form onSubmit={
+                    submitNewComment
+
+                }>
+
+                    <input name="comment" type="text"
+                           placeholder="Introduce tu comentario"  onChange={handleChange}></input>
+                    <button type = 'submit' placeholder='Enviar' value = 'Submit'>Enviar</button>
+                </form>
+                <div>
+                    {comment.map(comment => (
+                        <div>
+                            <h5>{`${user.nombre} escribió el ${formatDate}`}</h5>
+                            <p>{comment.comment}</p>
+                            <hr className="hrModal"></hr>
+                        </div>))
+                    }
+                </div>
+                <div className="PodcastsUser-wrap">
+                    <br/>
+                    {editPodcastLink()}
+                </div>
+            </div>
+        </>
+    );
 };
